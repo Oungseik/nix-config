5a952478e7c10239e1481f44ecb0e7933a8260bb
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-07-09 08:35:08 +0630

    feat: setup alacritty as home manager module

---
+++ home-managers/programs/alacritty.nix
@@ -0,0 +1,61 @@
+{ config, ... }:
+
+{
+  programs.alacritty.enable = true;
+  programs.alacritty.settings = {
+    env = {
+      TERM = "xterm-256color";
+    }; 
+
+    font = {
+      size = 13;
+      normal = {
+        family = "JetBrainsMono NF";
+        style = "Regular";
+      };
+      offset.x = 0;
+      offset.y = 0;
+    };
+
+    keyboard.bindings = {
+      action = "CreateNewWindow";
+      key = "N";
+      mods = "Shift|Control";
+    };
+
+    window = {
+      opacity = 0.95;
+    };
+
+    colors = with config.colorScheme.colors; {
+      bright = {
+        black = "0x${base00}";
+        blue = "0x${base0D}";
+        cyan = "0x${base0C}";
+        green = "0x${base0B}";
+        magenta = "0x${base0E}";
+        red = "0x${base08}";
+        white = "0x${base06}";
+        yellow = "0x${base09}";
+      };
+      cursor = {
+        cursor = "0x${base06}";
+        text = "0x${base06}";
+      };
+      normal = {
+        black = "0x${base00}";
+        blue = "0x${base0D}";
+        cyan = "0x${base0C}";
+        green = "0x${base0B}";
+        magenta = "0x${base0E}";
+        red = "0x${base08}";
+        white = "0x${base06}";
+        yellow = "0x${base0A}";
+      };
+      primary = {
+        background = "0x${base00}";
+        foreground = "0x${base06}";
+      };
+    };
+  };
+}
--- users/oung/home.nix
+++ users/oung/home.nix
@@ -14,6 +14,7 @@
   wayland.windowManager.hyprland.enable = true;
 
   programs = {
+    alacritty.enable = true;
     firefox.enable = true;
     git.enable = true;
     starship.enable = true;
