5dbdca6ac3bddce672734aacc2a9caa4133012f1
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-07-12 19:52:24 +0630

    disable separators and change icons

--- modules/programs/nixvim/plugins/options.nix
+++ modules/programs/nixvim/plugins/options.nix
@@ -1,10 +1,9 @@
 { ... }: {
   programs.nixvim = {
     extraConfigLuaPre =
-      # lua
       ''
-        vim.fn.sign_define("diagnosticsignerror", { text = " ", texthl = "diagnosticerror", linehl = "", numhl = "" })
-        vim.fn.sign_define("diagnosticsignwarn", { text = " ", texthl = "diagnosticwarn", linehl = "", numhl = "" })
+        vim.fn.sign_define("diagnosticsignerror", { text = " ", texthl = "diagnosticwarn", linehl = "", numhl = "" })
+        vim.fn.sign_define("diagnosticsignwarn", { text = " ", texthl = "diagnosticerror", linehl = "", numhl = "" })
         vim.fn.sign_define("diagnosticsignhint", { text = "󰌵", texthl = "diagnostichint", linehl = "", numhl = "" })
         vim.fn.sign_define("diagnosticsigninfo", { text = " ", texthl = "diagnosticinfo", linehl = "", numhl = "" })
       '';
