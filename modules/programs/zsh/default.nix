5431da3b7d474f7d809cf5df54e0e669651f4cd8
oungseik <mhemaungthuwin@gmail.com>
2024-07-14 19:38:17 +0630

    configure zsh with fzf

--- modules/programs/zsh/default.nix
+++ modules/programs/zsh/default.nix
@@ -1,12 +1,26 @@
-{ ... }: {
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ exa ];
   programs.zsh = {
+    autocd = true;
+    dotDir = ".config/zsh";
     enableCompletion = true;
     syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ls = "exa";
+      la = "exa -la";
+
+    };
 
     history = {
       size = 10000;
       path = "$HOME/.zsh/zsh_history";
     };
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
   };
 
   home.file.".zsh" = {
