c9b306aaa3f43b04df23d2c9cb1e14e09f2104d1
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-07-17 19:11:18 +0630

    feat: change roboto font and update flake

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -4,7 +4,7 @@
     enable = true;
     theme = {
       package = pkgs.adw-gtk3;
-      name = "adw-gtk3";
+      name = "adw-gtk3-dark";
     };
 
     cursorTheme = {
@@ -16,14 +16,14 @@
     # iconTheme.name = "GruvboxPlus";
 
     font = {
-      name = "RobotoCondensed";
+      name = "Roboto";
       size = 12;
     };
   };
 
   qt = {
     enable = true;
-    platformTheme = "gtk";
+    platformTheme.name = "gtk";
 
     style.name = "adwaita-dark";
     style.package = pkgs.adwaita-qt;
