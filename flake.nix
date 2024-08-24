1610571fc00b8a8a4e04ec3bfd0c9344c648f871
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-08-24 19:35:51 +0630

    fix: change the source of nixvim

--- flake.nix
+++ flake.nix
@@ -8,45 +8,58 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nixvim.url = "github:Oungseik/nixvim";
+    nixvim.url = "github:Oungseik/nixvim-experimental";
     nix-colors.url = "github:misterio77/nix-colors";
     # helix.url = "github:helix-editor/helix/master";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-    nixosConfigurations = {
-      "msi-modern15-A5M" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/msi-modern15-A5M/configuration.nix
-          # you can use this instead of `specialArgs`. Check out https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-and-module-system#pass-non-default-parameters-to-submodules 
-          # { _module.args = { inherit inputs; }; }
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        "msi-modern15-A5M" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            ./hosts/msi-modern15-A5M/configuration.nix
+            # you can use this instead of `specialArgs`. Check out https://nixos-and-flakes.thiscute.world/nixos-with-flakes/nixos-flake-and-module-system#pass-non-default-parameters-to-submodules 
+            # { _module.args = { inherit inputs; }; }
 
-          # # if you want to enable home manager as nixos module, enable this. but you need to disable the home configuration 
-          # # otherwise you will get recursion error
-          # home-manager.nixosModules.home-manager {
-          #   home-manager.useGlobalPkgs = true;
-          #   home-manager.useUserPackages = true;
-          #   home-manager.users.oung = import ./home/oung.nix;
-          # }
-        ];
+            # # if you want to enable home manager as nixos module, enable this. but you need to disable the home configuration 
+            # # otherwise you will get recursion error
+            # home-manager.nixosModules.home-manager {
+            #   home-manager.useGlobalPkgs = true;
+            #   home-manager.useUserPackages = true;
+            #   home-manager.users.oung = import ./home/oung.nix;
+            # }
+          ];
+        };
+
+        "asus-series-A56C" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [ ./hosts/asus-series-A56C/configuration.nix ];
+        };
+
       };
 
-      "asus-series-A56C" = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [ ./hosts/asus-series-A56C/configuration.nix ];
+      homeConfigurations = {
+        oung = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = {
+            inherit inputs;
+          };
+          modules = [ ./home/oung.nix ];
+        };
       };
-
     };
-
-    homeConfigurations = {
-      oung = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [ ./home/oung.nix ];
-      };
-    };
-  };
 }
