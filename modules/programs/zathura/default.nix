2757e8e3cd007f62b86e57d5742c90c92fb4e6a2
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2024-09-02 10:09:39 +0630

    remove zoom in and out with o and i

--- modules/programs/zathura/default.nix
+++ modules/programs/zathura/default.nix
@@ -1,9 +1,8 @@
-{ pkgs, ... }: {
+{ ... }:
+{
   programs.zathura = {
     enable = true;
     mappings = {
-      "[normal] <C-i>" = "zoom in";
-      "[normal] <C-o>" = "zoom out";
       "[normal] <C-=>" = "zoom in";
       "[normal] <C-->" = "zoom out";
     };
