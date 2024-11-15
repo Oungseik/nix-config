bf5392fa49f2d2320cb930128bb2441b78741b15
oungseik <mhemaungthuwin@gmail.com>
2024-11-15 14:20:55 +0630

    fix: remove unsuccessful build of picom

--- modules/window-managers/picom/default.nix
+++ modules/window-managers/picom/default.nix
@@ -1,15 +1,6 @@
 { pkgs, ... }:
 {
   services.picom = {
-    package = pkgs.picom.overrideAttrs (oldAttrs: {
-      src = pkgs.fetchFromGitHub {
-        owner = "ibhagwan";
-        repo = "picom";
-        rev = "c4107bb6cc17773fdc6c48bb2e475ef957513c7a";
-        hash = "sha256-1hVFBGo4Ieke2T9PqMur1w4D0bz/L3FAvfujY9Zergw=";
-      };
-    });
-
     backend = "glx";
 
     # activeOpacity = 1.0;
