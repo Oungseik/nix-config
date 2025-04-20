bd377a18d37b3ef7063b2921c1cb18b98ebe5f89
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-04-20 07:14:02 +0630

    chore: update yazi plugins and add yamb

--- modules/cli/yazi/default.nix
+++ modules/cli/yazi/default.nix
@@ -3,22 +3,22 @@ let
   plugins-repo = pkgs.fetchFromGitHub {
     owner = "yazi-rs";
     repo = "plugins";
-    rev = "c7feb90930a20ac8782305ed28ab4d9715124833";
-    hash = "sha256-Q+i1U+2uNlThf3+fFMUkWk9m7ZJ4z0mwVsrmqPFKVcE=";
+    rev = "b12a9ab085a8c2fe2b921e1547ee667b714185f9";
+    hash = "sha256-LWN0riaUazQl3llTNNUMktG+7GLAHaG/IxNj1gFhDRE=";
   };
   starship = pkgs.fetchFromGitHub {
     owner = "Rolv-Apneseth";
     repo = "starship.yazi";
-    rev = "9c37d37099455a44343f4b491d56debf97435a0e";
-    sha256 = "sha256-wESy7lFWan/jTYgtKGQ3lfK69SnDZ+kDx4K1NfY4xf4=";
+    rev = "c0707544f1d526f704dab2da15f379ec90d613c2";
+    sha256 = "sha256-H8j+9jcdcpPFXVO/XQZL3zq1l5f/WiOm4YUxAMduSRs=";
+  };
+  yamb = pkgs.fetchFromGitHub {
+    owner = "h-hg";
+    repo = "yamb.yazi";
+    rev = "22af0033be18eead7b04c2768767d38ccfbaa05b";
+    hash = "sha256-NMxZ8/7HQgs+BsZeH4nEglWsRH2ibAzq7hRSyrtFDTA=";
   };
 in
-# yamb = pkgs.fetchFromGitHub {
-#   owner = "h-hg";
-#   repo = "yamb.yazi";
-#   rev = "0598b8defc21ac7231e584115c3aa330f0310db4";
-#   hash = "sha256-Mw+CUyy8sN76NJNy+Xt0HwH6tD/gxqNvyIYKqpWUKMs=";
-# };
 {
 
   programs.yazi = {
@@ -28,27 +28,68 @@ in
       manager = {
         show_hidden = false;
       };
-      preview = {
-        max_width = 1000;
-        max_height = 1000;
-      };
+      # preview = {
+      # max_width = 1000;
+      # max_height = 1000;
+      # };
     };
 
     theme = import ./themes/catppuccin.nix;
 
     plugins = {
       inherit starship;
-      # inherit yamb;
+      inherit yamb;
       full-border = "${plugins-repo}/full-border.yazi";
     };
 
+    keymap = {
+      manager.prepend_keymap = import ./keymaps/yamb.nix;
+    };
+
     initLua = ''
+      require("starship"):setup()
       require("full-border"):setup {
       	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
       	type = ui.Border.ROUNDED,
       }
 
-      require("starship"):setup()
+      -- You can configure your bookmarks by lua language
+      local bookmarks = {}
+
+      local path_sep = package.config:sub(1, 1)
+      local home_path = ya.target_family() == "windows" and os.getenv("USERPROFILE") or os.getenv("HOME")
+      if ya.target_family() == "windows" then
+        table.insert(bookmarks, {
+          tag = "Scoop Local",
+          
+          path = (os.getenv("SCOOP") or home_path .. "\\scoop") .. "\\",
+          key = "p"
+        })
+        table.insert(bookmarks, {
+          tag = "Scoop Global",
+          path = (os.getenv("SCOOP_GLOBAL") or "C:\\ProgramData\\scoop") .. "\\",
+          key = "P"
+        })
+      end
+      table.insert(bookmarks, {
+        tag = "Desktop",
+        path = home_path .. path_sep .. "Desktop" .. path_sep,
+        key = "d"
+      })
+
+      require("yamb"):setup {
+        -- Optional, the path ending with path seperator represents folder.
+        bookmarks = bookmarks,
+        -- Optional, recieve notification everytime you jump.
+        jump_notify = true,
+        -- Optional, the cli of fzf.
+        cli = "fzf",
+        -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+        keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        -- Optional, the path of bookmarks
+        path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or
+              (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+      }
     '';
   };
 }
