e5f254adf2a6a2826cea79040d3a0a7fe7b03ad4
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-05-14 07:00:54 +0630

    fix: remove deprecated api

--- modules/cli/zsh/default.nix
+++ modules/cli/zsh/default.nix
@@ -32,7 +32,7 @@
       # }
     ];
 
-    initExtra = ''
+    initContent = ''
       bindkey -e
       bindkey '^P' history-beginning-search-backward
       bindkey '^N' history-beginning-search-forward
