ade1b5a44fbd254dcd75193d88053c2fd0fba42a
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-05-14 07:01:42 +0630

    style: change mako style

--- modules/notification/mako/default.nix
+++ modules/notification/mako/default.nix
@@ -8,17 +8,22 @@
     borderSize = 2;
 
     font = "JetBrainsMono NF 11";
-    padding = "12";
+    padding = "5";
 
     backgroundColor = "#${base01}";
     textColor = "#${base05}"; # maybe base04
     borderColor = "#${base0D}";
     progressColor = "over #${base02}";
     maxIconSize = 40;
+    # defaultTimeout = 20000;
+    groupBy = "summary";
 
     extraConfig = ''
       [urgency=high]
       border-color=#${base09}
+
+      [grouped]
+      format=<b>%s</b>\n%b
     '';
   };
 }
