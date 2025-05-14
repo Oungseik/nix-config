5485e38817abf0a4d0bac6483afa9c673b793dcd
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-05-14 07:56:37 +0630

    update flake and fix warning config

--- modules/notification/mako/default.nix
+++ modules/notification/mako/default.nix
@@ -1,29 +1,23 @@
 { config, ... }:
 {
   services.mako = with config.colorScheme.palette; {
-    # anchor = "top-right";
-    layer = "overlay";
+    settings = {
+      # anchor = "top-right";
+      layer = "overlay";
 
-    borderRadius = 4;
-    borderSize = 2;
+      border-radius = 4;
+      border-size = 2;
 
-    font = "JetBrainsMono NF 11";
-    padding = "5";
+      font = "JetBrainsMono NF 11";
+      padding = "5";
 
-    backgroundColor = "#${base01}";
-    textColor = "#${base05}"; # maybe base04
-    borderColor = "#${base0D}";
-    progressColor = "over #${base02}";
-    maxIconSize = 40;
-    # defaultTimeout = 20000;
-    groupBy = "summary";
-
-    extraConfig = ''
-      [urgency=high]
-      border-color=#${base09}
-
-      [grouped]
-      format=<b>%s</b>\n%b
-    '';
+      background-color = "#${base01}";
+      text-color = "#${base05}"; # maybe base04
+      border-color = "#${base0D}";
+      progress-color = "over #${base02}";
+      max-icon-size = 40;
+      # default-timeout = 20000;
+      group-by = "summary";
+    };
   };
 }
