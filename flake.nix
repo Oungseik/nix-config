75f3baf75a3e0b29860d136b4b9fd8fa5663cb47
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-07-19 01:42:00 +0630

    chore: remove unused home configuration

--- flake.nix
+++ flake.nix
@@ -60,14 +60,6 @@
           };
           modules = [ ./home/oung.nix ];
         };
-
-        saitama = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          extraSpecialArgs = {
-            inherit inputs;
-          };
-          modules = [ ./home/saitama.nix ];
-        };
       };
     };
 }
