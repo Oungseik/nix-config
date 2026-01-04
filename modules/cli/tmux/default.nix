39fcba5996b6bd7823a20f56712188990ab96a87
Oung Seik <mhemaungthuwin@gmail.com>
2026-01-04 19:04:43 +0630

    feat: configure tmux

--- modules/cli/tmux/default.nix
+++ modules/cli/tmux/default.nix
@@ -10,26 +10,39 @@
 
     plugins = with pkgs; [
       tmuxPlugins.sensible
-      tmuxPlugins.catppuccin
       tmuxPlugins.vim-tmux-navigator
+      tmuxPlugins.tmux-nova
     ];
 
     extraConfig = ''
-      set -ag terminal-overrides ",alacritty:RGB"
-      set -ga terminal-overrides ",xterm-256color*:Tc"
-      set -g focus-events on
-
       bind -T copy-mode-vi v send -X begin-selection
       bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel
 
-      # bind -n C-H previous-window
-      # bind -n C-L next-window
-      bind-key -n M-Space resize-pane -Z
+      set -g @nova-nerdfonts false
 
-      # catppuccin configs
-      set -g @catppuccin_window_default_text '#W'
-      set -g @catppuccin_window_current_text '#W'
-      set -g @catppuccin_status_background 'none'
+      set -g @nova-pane "#{?window_active,,}#I:#W"
+
+      set -g @nova-pane-active-border-style "#44475a"
+      set -g @nova-pane-border-style "#282a36"
+      set -g @nova-status-style-bg "#24273a"
+      set -g @nova-status-style-fg "#f5a97f" # default pane color
+
+      set -g @nova-status-style-active-bg "#24273a"
+      set -g @nova-status-style-active-fg "#91d7e3"
+      set -g @nova-status-style-double-bg "#24273a"
+
+      set -g @nova-segment-os " ::"
+      set -g @nova-segment-os-colors "#24273a #8aadf4"
+
+      set -g @nova-segment-computer " :#h"
+      set -g @nova-segment-computer-colors "#24273a #c6a0f6"
+
+      set -g @nova-segment-session " :#S"
+      set -g @nova-segment-session-colors "#24273a #8aadf4"
+
+      set -g @nova-rows 0
+      set -g @nova-segments-0-left "os"
+      set -g @nova-segments-0-right "computer session"
     '';
   };
 }
