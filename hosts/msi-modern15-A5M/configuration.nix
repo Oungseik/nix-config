9f880b29e3d01a3f3a9441c8b7b047f8337ab44b
Oung Seik <mhemaungthuwin@gmail.com>
2026-07-01 13:20:24 +0630

    chore: update flake and cleanup proxy

--- hosts/msi-modern15-A5M/configuration.nix
+++ hosts/msi-modern15-A5M/configuration.nix
@@ -63,34 +63,6 @@
       "8.8.4.4"
     ];
     # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-    # Configure network proxy if necessary
-    # proxy.default = "http://127.0.0.1:2080/";
-    proxy.noProxy = lib.strings.concatStringsSep "," [
-      "127.0.0.1"
-      "192.168.0.0/16"
-      "103.186.240.90"
-      "localhost"
-      "internal.domain"
-      "youtube.com"
-      "teams.microsoft.com"
-      "microsoft.com"
-      "reddit.com"
-
-      "cloud.langfuse.com"
-      "api.openai.com"
-      "openai.com"
-      "api-inference.huggingface.co"
-      "huggingface.co"
-      "registry.npmjs.org"
-      "npmjs.org"
-      "api.cohere.com"
-      "cohere.com"
-      "generativelanguage.googleapis.com"
-      "googleapis.com"
-      "api.anthropic.com"
-      "anthropic.com"
-    ];
   };
 
   # Set your time zone.
