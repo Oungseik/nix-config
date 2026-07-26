aee36b9322e094ec886799fa5a4487b136825ccc
Oung Seik <mhemaungthuwin@gmail.com>
2026-07-26 13:11:55 +0630

    configure Qt icon theme

--- modules/themes/default.nix
+++ modules/themes/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   gtk = {
     enable = true;
@@ -12,8 +12,10 @@
       name = "Bibata-Modern-Ice";
     };
 
-    iconTheme.package = pkgs.kora-icon-theme;
-    iconTheme.name = "Kora";
+    iconTheme = {
+      package = pkgs.kora-icon-theme;
+      name = "Kora";
+    };
 
     font = {
       name = "Roboto";
@@ -23,9 +25,16 @@
 
   qt = {
     enable = true;
-    platformTheme.name = "gtk3";
+    platformTheme.name = "qtct";
+    qt5ctSettings.Appearance.icon_theme = config.gtk.iconTheme.name;
+    qt6ctSettings.Appearance.icon_theme = config.gtk.iconTheme.name;
 
     style.name = "adwaita-dark";
     style.package = pkgs.adwaita-qt;
+  };
+
+  xdg.configFile = {
+    "qt5ct/qt5ct.conf".force = true;
+    "qt6ct/qt6ct.conf".force = true;
   };
 }
