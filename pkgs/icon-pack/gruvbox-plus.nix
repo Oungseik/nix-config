eceacf2609d712f3be38404f60b9f81c33b86bac
Oung Seik <mhemaungthuwin@gmail.com>
2026-07-26 13:02:37 +0630

    upgrade gruvbox-plus icons

--- pkgs/icon-pack/gruvbox-plus.nix
+++ pkgs/icon-pack/gruvbox-plus.nix
@@ -4,8 +4,8 @@ pkgs.stdenv.mkDerivation {
   name = "gruvbox-plus";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/SylEleuth/gruvbox-plus-icon-pack/releases/download/v5.4.0/gruvbox-plus-icon-pack-5.4.zip";
-    sha256 = "1zpr9ha5x3546ms8gqhzs8j7inhz1j4disap7s897zmfipj88zd5";
+    url = "https://github.com/SylEleuth/gruvbox-plus-icon-pack/releases/download/v6.5.0/gruvbox-plus-icon-pack-6.5.0.zip";
+    sha256 = "1sqffkypjs9x0sgivw6f4d9blz9simbaklqgx5q5jy37fb8621ji";
   };
 
   dontUnpack = true;
